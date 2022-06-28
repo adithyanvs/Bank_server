f9802676bf3cab40b623001f5f4860131a59d85f
adithyanvs <adithyanvs@gmail.com>
2022-06-28 13:30:42 +0530

    server_completion

--- index.js
+++ index.js
@@ -2,20 +2,27 @@
 
 //1. Import express
 
-const express =  require('express')
+const express = require('express')
 //import jsonwebtoken
 const jwt = require('jsonwebtoken')
+//import cors
+const cors= require('cors')
 
-const dataService =require('./services/data.service') //used to import dataservices into index.js
+const dataService = require('./services/data.service') //used to import dataservices into index.js
 
 //server app creation using express
 const app = express()
+
+//cors use in server app
+app.use(cors({
+    origin:'http://localhost:4200/'
+}))
 
 //parse JSON data
 app.use(express.json())//used to convert requested JSON formate data into string or defined formate
 
 //application specific middleware
-const appMiddleware = (req,res,next) => {
+const appMiddleware = (req, res, next) => {
     console.log("application specific middleware");
     next()
 }
@@ -25,81 +32,103 @@ app.use(appMiddleware)
 
 //Bank server
 
-const jwtMiddleware =(req,res,next) => {
+const jwtMiddleware = (req, res, next) => {
     //fetch token
-    token = req.body.token
-    //verfy token
-   const data = jwt.verify(token,'supersecretkey12345')
-   console.log(data);
-   next()
-
-
+    try {
+        token = req.headers['x-access-token'] //token = req.body.token =>token given in body section or token given in header secion
+        //verfy token
+        const data = jwt.verify(token, 'supersecretkey12345')
+        console.log(data);
+        next()
+    }
+    catch {
+        res.status(401).json({
+            status: false,
+            statusCode: 401,
+            message: 'Please Log In'
+        })
+    }
 }
 //1.Register API
-app.post('/register',(req,res) =>{
+app.post('/register', (req, res) => {
     //register solving here....>
     // console.log(req);
-   const result = dataService.regiser(req.body.username,req.body.acno,req.body.password)
-   res.status(result.statusCode).json(result)
-//    if(result){
-//     res.send("Registered successfully")
-//    }
-//    else{
-//     res.send("Already resgistered...please log in")
-//    }
+    dataService.regiser(req.body.username, req.body.acno, req.body.password)
+        .then(result => {
+            res.status(result.statusCode).json(result)
+        })
+
+    //    if(result){
+    //     res.send("Registered successfully")
+    //    }
+    //    else{
+    //     res.send("Already resgistered...please log in")
+    //    }
 
 })
 //Login API creation
-app.post('/login',(req,res) =>{
+app.post('/login', (req, res) => {
     //Login solving here....>
-   const result = dataService.login(req.body.acno,req.body.password,)
-   res.status(result.statusCode).json(result)
+    dataService.login(req.body.acno, req.body.password,)
+    .then(result => {
+        res.status(result.statusCode).json(result)
+    })
+    
 })
 //deposit API creation
-app.post('/deposit',jwtMiddleware,(req,res) =>{
+app.post('/deposit', jwtMiddleware, (req, res) => {
     //Login solving here....>
-   const result = dataService.deposit(req.body.acno,req.body.password,req.body.amt)
-   res.status(result.statusCode).json(result)
+    dataService.deposit(req.body.acno, req.body.password, req.body.amt)
+    .then(result => {
+        res.status(result.statusCode).json(result)
+    })
+    
 })
 //withdraw API
-app.post('/withdraw',(req,res) =>{
+app.post('/withdraw', jwtMiddleware, (req, res) => {
     //Login solving here....>
-   const result = dataService.withdraw(req.body.acno,req.body.password,req.body.amt)
-   res.status(result.statusCode).json(result)
+    dataService.withdraw(req.body.acno, req.body.password, req.body.amt)
+    .then(result => {
+        res.status(result.statusCode).json(result)
+    })
+    
 })
 //Get transaction API
-app.post('/transaction',(req,res) =>{
+app.post('/transaction', jwtMiddleware, (req, res) => {
     //Login solving here....>
-   const result = dataService.getTrasaction(req.body.acno)
-   res.status(result.statusCode).json(result)
+    dataService.getTrasaction(req.body.acno)
+    .then(result => {
+        res.status(result.statusCode).json(result)
+    })
+    
 })
 
 
 
 //user request resolving
 //GET request:-to fetch data
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send("GET Request")
 })
 //POST request:-to create data
-app.post('/',(req,res) =>{
+app.post('/', (req, res) => {
     res.send("POST request")
 })
 //PUT request:-to modify entire data
-app.put('/',(req,res) =>{
+app.put('/', (req, res) => {
     res.send("PUT request")
 })
 //PATCH request:-to modify partially
-app.patch('/',(req,res) =>{
+app.patch('/', (req, res) => {
     res.send("PATCH request")
 })
 //DELETE rquest:-to delete data
-app.delete('/',(req,res) =>{
+app.delete('/', (req, res) => {
     res.send("DELETE rquest")
 })
 
 
 //set up port number to the server app
-app.listen(3000,() =>{
+app.listen(3000, () => {
     console.log("Server started at 3000");
 })
--- services/data.service.js
+++ services/data.service.js
@@ -1,43 +1,82 @@
 //import jsonwebtoken
 const jwt = require('jsonwebtoken')
+
+//impor db.js here
+const db = require('./db')
+
 //DATABASE
 //below key value pair is called dictionary
-db = {
-  1000: { "acno": 1000, "username": "ram", "password": 1000, "balance": 5000, transaction: [] },
-  1001: { "acno": 1001, "username": "tom", "password": 1001, "balance": 6000, transaction: [] },
-  1002: { "acno": 1002, "username": "hari", "password": 1002, "balance": 3000, transaction: [] },
-}
-//rgister
+// db ={
+//   1000: { "acno": 1000, "username": "ram", "password": 1000, "balance": 5000, transaction: [] },
+//   1001: { "acno": 1001, "username": "tom", "password": 1001, "balance": 6000, transaction: [] },
+//   1002: { "acno": 1002, "username": "hari", "password": 1002, "balance": 3000, transaction: [] },
+// }
+//register
 
 const regiser = (username, acno, password) => {
-  if (acno in db) {
-    return {
-      status: false,
-      message: "Already resgistered...please log in",
-      statusCode: 401//unauthorized entity code
+  return db.User.findOne({
+    acno
+  }).then(user => {
+    if (user) {
+      return {
+        status: false,
+        message: "Already resgistered...please log in",
+        statusCode: 401//unauthorized entity code
+      }
     }
-  }
-  else {
-    db[acno] = { acno, username, password, "balance": 0, transaction: [] }
-    return {
-      status: true,
-      message: "Registered successfully",
-      statusCode: 200
+    else {
+      //insert in db
+      const newUser = new db.User({
+        acno,
+        username,
+        password,
+        balance: 0,
+        transaction: []
+
+      })
+      newUser.save()
+      return {
+        status: true,
+        message: "Registered successfully",
+        statusCode: 200
+      }
     }
-  }
+
+
+
+  })
+  // if (acno in db) {
+  // return {
+  //   status: false,
+  //   message: "Already resgistered...please log in",
+  //   statusCode: 401//unauthorized entity code
+  // }
+  // }
+  // else {
+  //   db[acno] = { acno, username, password, "balance": 0, transaction: [] }
+  //   return {
+  //     status: true,
+  //     message: "Registered successfully",
+  //     statusCode: 200
+  //   }
+  // }
 }
 
-//login
+//login -asynchronous
 const login = (acno, pswd) => {
-  if (acno in db) {
-    if (pswd == db[acno]["password"]) {
-      currentUser= db[acno]["username"]
+
+  return db.User.findOne({
+    acno,
+    password: pswd
+  }).then(user => {
+    if (user) {
+      currentUser = user.username
       currentAcno = acno
       //token generation
-     token = jwt.sign({
+      token = jwt.sign({
         //store account number inside token
-        currentAcno:acno//key value to be stored
-      },'supersecretkey12345')
+        currentAcno: acno//key value to be stored
+      }, 'supersecretkey12345')
 
       return {
         status: true,
@@ -47,115 +86,223 @@ const login = (acno, pswd) => {
         currentAcno,
         token
       }
-    }
-    else {
-      return {
-        status: false,
-        message: "Incorrect password",
-        statusCode: 401//unauthorized entity code
-      }
-    }
-  }
-  else {
-    return {
-      status: false,
-      message: "user does not exit!!!",
-      statusCode: 401//unauthorized entity code
-    }
-  }
-}
-
-  //deposit
-
-  const deposit=(acno, password, amt) => {
-    var amount = parseInt(amt)
-    if (acno in db) {
-      if (password == db[acno]["password"]) {
-        db[acno]["balance"] += amount
-        db[acno].transaction.push({
-          type:"CREDIT",
-          amount:amount
-        })
-        return {
-          status: true,
-          message: amount+ " Deposied Successfully..New Balance is"+db[acno]["balance"],
-          statusCode: 200//succes code
-        }
-       
-      } else {
-        return {
-          status: false,
-          message: "Incorrect password",
-          statusCode: 401//unauthorized entity code
-        }
-      }
 
     } else {
       return {
         status: false,
-        message: "User does not exist.....",
+        message: "Invalid Account number or password",
         statusCode: 401//unauthorized entity code
       }
     }
-  }
+  })
+  // if (acno in db) {
+  //   if (pswd == db[acno]["password"]) {
+  //     currentUser = db[acno]["username"]
+  //     currentAcno = acno
+  //     //token generation
+  //     token = jwt.sign({
+  //       //store account number inside token
+  //       currentAcno: acno//key value to be stored
+  //     }, 'supersecretkey12345')
 
- //withdraw
-  const withdraw = (acno, password, amt) => {
+  //     return {
+  //       status: true,
+  //       message: "Login Succesful",
+  //       statusCode: 200,//succes code
+  //       currentUser,
+  //       currentAcno,
+  //       token
+  //     }
+  //   }
+  //   else {
+  //     return {
+  //       status: false,
+  //       message: "Incorrect password",
+  //       statusCode: 401//unauthorized entity code
+  //     }
+  //   }
+  // }
+  // else {
+  //   return {
+  //     status: false,
+  //     message: "user does not exit!!!",
+  //     statusCode: 401//unauthorized entity code
+  //   }
+  // }
+}
+
+//deposit
+
+const deposit = (acno, password, amt) => {
   var amount = parseInt(amt)
-  if (acno in db) {
-    if (password == db[acno]["password"]) {
-      if (db[acno]["balance"] > amount) {
-        db[acno]["balance"] -= amount
-        db[acno].transaction.push({
-          type:"DEBIT",
-          amount:amount
+  return db.User.findOne({
+    acno, password
+  }).then(user => {
+
+
+    if (user) {
+      user.balance += amount
+      user.transaction.push({
+        type: "CREDIT",
+        amount: amount
+      })
+      user.save()
+      return {
+        status: true,
+        message: amount + " Deposied Successfully..New Balance is" + user.balance,
+        statusCode: 200//succes code
+      }
+    } else {
+      return {
+        status: false,
+        message: "Invalid Account number or password",
+        statusCode: 401//unauthorized entity code
+      }
+    }
+  })
+
+  // if (acno in db) {
+  //   if (password == db[acno]["password"]) {
+  //     db[acno]["balance"] += amount
+  //     db[acno].transaction.push({
+  //       type: "CREDIT",
+  //       amount: amount
+  //     })
+  //     return {
+  //       status: true,
+  //       message: amount + " Deposied Successfully..New Balance is" + db[acno]["balance"],
+  //       statusCode: 200//succes code
+  //     }
+
+  //   } else {
+  //     return {
+  //       status: false,
+  //       message: "Incorrect password",
+  //       statusCode: 401//unauthorized entity code
+  //     }
+  //   }
+
+  // } else {
+  //   return {
+  //     status: false,
+  //     message: "User does not exist.....",
+  //     statusCode: 401//unauthorized entity code
+  //   }
+  // }
+}
+
+//withdraw
+const withdraw = (acno, password, amt) => {
+  var amount = parseInt(amt)
+  return db.User.findOne({
+    acno, password
+  }).then(user => {
+    if (user) {
+      if (user.balance > amount) {
+        user.balance -= amount
+        user.transaction.push({
+          type: "debit",
+          amount: amount
         })
+        user.save()
         return {
+
           status: true,
-          message: amount+ " Debitted Successfully..New Balance is"+db[acno]["balance"],
+          message: amount + " Debitted Successfully..New Balance is" + user.balance,
           statusCode: 200//succes code
+
         }
-        
-      } else {
+      }
+      else {
         return {
           status: false,
           message: "Insufficient balance",
           statusCode: 422//unprocesseble entity code
         }
       }
-
-    } else {
+    }
+    else {
       return {
         status: false,
-        message: "Incorrect password",
+        message: "User does not exist.....",
         statusCode: 401//unauthorized entity code
       }
     }
+  })
+  }
 
-  } else {
-    return  {
-      status: false,
-      message: "User does not exist.....",
-      statusCode: 401//unauthorized entity code
-    }
-  }
-}
+// if (acno in db) {
+//   if (password == db[acno]["password"]) {
+//     if (db[acno]["balance"] > amount) {
+//       db[acno]["balance"] -= amount
+//       db[acno].transaction.push({
+//         type: "DEBIT",
+//         amount: amount
+//       })
+//       return {
+//         status: true,
+//         message: amount + " Debitted Successfully..New Balance is" + db[acno]["balance"],
+//         statusCode: 200//succes code
+//       }
+
+//     } else {
+//       return {
+//         status: false,
+//         message: "Insufficient balance",
+//         statusCode: 422//unprocesseble entity code
+//       }
+//     }
+
+//   } else {
+//     return {
+//       status: false,
+//       message: "Incorrect password",
+//       statusCode: 401//unauthorized entity code
+//     }
+//   }
+
+// } else {
+//   return {
+//     status: false,
+//     message: "User does not exist.....",
+//     statusCode: 401//unauthorized entity code
+//   }
+
+
 //Get transaction
-const getTrasaction=(acno) =>{
-  if(acno in db){
-    return{
-      staus:true,
-      statusCode:200,
-      transaction:db[acno].transaction 
+const getTrasaction = (acno) => {
+  return db.User.findOne({
+    acno
+  }).then(user => {
+    if(user){
+      return {
+        staus: true,
+        statusCode: 200,
+        transaction: user.transaction
+      }
+    }else{
+      return {
+        status: false,
+        message: "user does not exist!!",
+        statusCode: 401
+      }
+
     }
-  }else{
-    return{
-      status:false,
-      message:"user does not exist!!",
-      statusCode:401
-    }
-  }
-  
+  })
+  // if (acno in db) {
+  //   return {
+  //     staus: true,
+  //     statusCode: 200,
+  //     transaction: db[acno].transaction
+  //   }
+  // } else {
+  //   return {
+  //     status: false,
+  //     message: "user does not exist!!",
+  //     statusCode: 401
+  //   }
+  // }
+
 }
 
 //export
